2752e85b5ba806f5cc1015f5e06a184da0caf4f0
TrofimovVladislav <arda.yug@gmail.com>
2022-10-14 18:54:27 +0300

    End task 5

--- PythonLection/lection_1.py
+++ PythonLection/lection_1.py
@@ -273,3 +273,86 @@
 # print(type(f(1))) # str
 # print(type(f(2.3))) # int
 # print(type(f(28))) # NoneType
+
+
+# Задача 5 Семинар 1
+# Задача 5 VERY HARD SORT необязательная
+
+# Задайте двумерный массив из целых чисел. Количество строк и столбцов задается 
+# с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+
+# Например, задан массив:
+# 1 4 7 2
+# 5 9 10 3
+
+# После сортировки
+# 1 2 3 4
+# 5 7 9 10
+
+# arr = [1, 4, 7, 2], [5, 9, 10, 3]
+# print(arr)
+# err_sor = sorted(arr[0] + arr[1])
+# a = int((len(err_sor)/2))
+# wer = err_sor[:a], err_sor[a:]
+# print (wer)
+
+# def chunks(n):
+#     return [l[i:i+n] for i in range(0, len(l), n)]
+
+# l = [1,2,3,4,5,6,7,8]
+# while 1:
+#     try:
+#         # n = int(input('What size? '))
+#         break
+#     except ValueError:
+#         print ('Numbers only please')
+
+# print(chunks(int(len(l)/2)))
+
+# size = int(input())
+# array_input = []
+# for x in range(size): 
+#     array_input.append([int(y) for y in input().split()])
+#     print(array_input)
+
+
+# wer = [wer[0], wer[1]]
+# def custom_key(people): 
+#     return people[0] # second parameter denotes the age
+
+# persons = [[1, 4, 7, 2], [5, 9, 10, 3]]
+# print(f'Before sorting: {persons}') 
+# persons.sort()
+# print(f'After sorting: {persons}')
+
+
+def create_array():
+    size = int(input('Введите размер массива: '))
+    array_input = [] 
+    for x in range(size): 
+        array_input.append([int(y) for y in input('Введите элементы массива: ').split()]) 
+    print(array_input)
+    return(array_input)
+    
+create_array()
+
+# size = int(input('Введите размер: '))
+# arr = int(input('Введите массив: '))
+# array = []
+# for x in range(size):
+#     for y in arr:
+#         array.append([int(arr)])
+# print(array)
+
+# 4. Выполнить алгоритм сортировки пузырьком
+# Это решение без temp, но можно и с temp, потом обязательно сделай.
+# list_numbers = [1, 8, 16, 3, 5, 9, 15, 1]
+# n = len(list_numbers)
+# for i in range(len(list_numbers) - 1):
+#     for j in range(n - i - 1):
+#         if list_numbers[j] > list_numbers[j + 1]:
+#             list_numbers[j], list_numbers[j + 1] = list_numbers[j + 1], list_numbers[j]
+# print(list_numbers)
+
+
+
--- PythonSeminars/HW/HW_Seminar_1/hw_sem_1_task_5.py
+++ PythonSeminars/HW/HW_Seminar_1/hw_sem_1_task_5.py
@@ -1,73 +1,28 @@
 # Задача 5 VERY HARD SORT необязательная
-
 # Задайте двумерный массив из целых чисел. Количество строк и столбцов задается 
 # с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
 
 # Например, задан массив:
 # 1 4 7 2
 # 5 9 10 3
-
 # После сортировки
 # 1 2 3 4
 # 5 7 9 10
 
-arr = [1, 4, 7, 2], [5, 9, 10, 3]
-print(arr)
-err_sor = sorted(arr[0] + arr[1])
-a = int((len(err_sor)/2))
-wer = err_sor[:a], err_sor[a:]
-print (wer)
+def create_array():
+    size = int(input('Введите размер массива: '))
+    array_input = [] 
+    for x in range(size): 
+        array_input.append([int(y) for y in input('Введите элементы массива через пробел: ').split()]) 
+    print(f'Исходный массив: {array_input}')
+    return array_input
 
-def chunks(n):
-    return [l[i:i+n] for i in range(0, len(l), n)]
+def sorted_2D_array():
+    array = create_array()
+    array_sorted = sorted(array[0] + array[1])
+    len_ = int((len(array_sorted)/2))
+    output_array = array_sorted[:len_], array_sorted[len_:]
+    print (f'Отсортированный массив: {output_array}\n')
+    return output_array
 
-l = [1,2,3,4,5,6,7,8]
-while 1:
-    try:
-        # n = int(input('What size? '))
-        break
-    except ValueError:
-        print ('Numbers only please')
-
-print(chunks(int(len(l)/2)))
-
-size = int(input())
-array_input = []
-for x in range(size): 
-    array_input.append([int(y) for y in input().split()])
-    print(array_input)
-
-
-# print(func)
-# print(func)
-
-# wer = [wer[0], wer[1]]
-# def custom_key(people): 
-#     return people[0] # second parameter denotes the age
-
-# persons = [[1, 4, 7, 2], [5, 9, 10, 3]]
-# print(f'Before sorting: {persons}') 
-# persons.sort() 
-# print(f'After sorting: {persons}')
-
-
-
-# size = int(input())
-# array_input = [] 
-# for x in range(size): 
-#     array_input.append([int(y) for y in input().split()]) 
-# print(array_input)
-
-# http://cs.mipt.ru/algo_fefm/lessons/lab6.html#section-3
-
-# 4. Выполнить алгоритм сортировки пузырьком
-# Это решение без temp, но можно и с temp, потом обязательно сделай.
-# list_numbers = [1, 8, 16, 3, 5, 9, 15, 1]
-# n = len(list_numbers)
-# for i in range(len(list_numbers) - 1):
-#     for j in range(n - i - 1):
-#         if list_numbers[j] > list_numbers[j + 1]:
-#             list_numbers[j], list_numbers[j + 1] = list_numbers[j + 1], list_numbers[j]
-# print(list_numbers)
-
-
+sorted_2D_array()
