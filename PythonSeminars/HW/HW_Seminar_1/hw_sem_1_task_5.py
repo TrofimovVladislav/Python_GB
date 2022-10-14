c3dc59128e983504070f6cfa4ce7fbe9651f844f
TrofimovVladislav <arda.yug@gmail.com>
2022-10-14 15:56:10 +0300

    Edited task 5

--- PythonSeminars/HW/HW_Seminar_1/hw_sem_1_task_5.py
+++ PythonSeminars/HW/HW_Seminar_1/hw_sem_1_task_5.py
@@ -11,14 +11,52 @@
 # 1 2 3 4
 # 5 7 9 10
 
-# arr = [1, 4, 7, 2], [5, 9, 10, 3]
-# print(arr)
+arr = [1, 4, 7, 2], [5, 9, 10, 3]
+print(arr)
+err_sor = sorted(arr[0] + arr[1])
+a = int((len(err_sor)/2))
+wer = err_sor[:a], err_sor[a:]
+print (wer)
+
+def chunks(n):
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+l = [1,2,3,4,5,6,7,8]
+while 1:
+    try:
+        # n = int(input('What size? '))
+        break
+    except ValueError:
+        print ('Numbers only please')
+
+print(chunks(int(len(l)/2)))
 
 size = int(input())
-array_input = [] 
+array_input = []
 for x in range(size): 
-    array_input.append([int(y) for y in input().split()]) 
-print(array_input)
+    array_input.append([int(y) for y in input().split()])
+    print(array_input)
+
+
+# print(func)
+# print(func)
+
+# wer = [wer[0], wer[1]]
+# def custom_key(people): 
+#     return people[0] # second parameter denotes the age
+
+# persons = [[1, 4, 7, 2], [5, 9, 10, 3]]
+# print(f'Before sorting: {persons}') 
+# persons.sort() 
+# print(f'After sorting: {persons}')
+
+
+
+# size = int(input())
+# array_input = [] 
+# for x in range(size): 
+#     array_input.append([int(y) for y in input().split()]) 
+# print(array_input)
 
 # http://cs.mipt.ru/algo_fefm/lessons/lab6.html#section-3
 
