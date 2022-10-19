d474376e4ec4f547c109ee4a7ff06f3bd833475b
TrofimovVladislav <arda.yug@gmail.com>
2022-10-19 08:48:31 +0300

    Edited task 5

--- PythonSeminars/HW/HW_Seminar_2/hw_sem_2_task_5.py
+++ PythonSeminars/HW/HW_Seminar_2/hw_sem_2_task_5.py
@@ -2,54 +2,56 @@
 # Но теперь количество предикатов не три, а генерируется случайным образом от 5 до 25, сами значения предикатов случайные, 
 # проверяем это утверждение 100 раз, с помощью модуля time выводим на экран , сколько времени отработала программа.
 
-import time
+# import time
 
-def Truth ():
-    result = True
-    start = time.time()
+# def Truth ():
+#     result = True
+#     start = time.time()
 
-    for n in range (0, 100):
-        num = bin(n)
-        print(num)
-        num = num.replace('b', '0')
-        X = int(num[-3])
-        # print(X)
-        Y = int(num[-2])
-        # print(Y)
-        Z = int(num [-1])
-        # print(Z)
-        result = (not(X or Y or Z)) == ((not X) and (not Y) and (not Z))
-        end = time.time()
-    print (result)
-    print(f'Время выполнения программы {end-start} сек')
+#     for n in range (0, 100):
+#         num = bin(n)
+#         print(num)
+#         num = num.replace('b', '0')
+#         X = int(num[-3])
+#         # print(X)
+#         Y = int(num[-2])
+#         # print(Y)
+#         Z = int(num [-1])
+#         # print(Z)
+#         result = (not(X or Y or Z)) == ((not X) and (not Y) and (not Z))
+#         end = time.time()
+#     print (result)
+#     print(f'Время выполнения программы {end-start} сек')
 
 
-Truth ()
+# Truth ()
 
 
 import random
 
-predict_lib = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-# pred = ' or '.join(con)
-# print(pred)
-n = random.randint(5, 26)
-
-pred_1 = random.sample(predict_lib, n)
-# print(pred_1)
-
-for i in pred_1:
-    # print(i, end=' ')
-    g = 'not'.join(i)
+# predict_lib = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+# n = random.randint(5, 6)
+# pred_1 = random.sample(predict_lib, n)
+# g = ('not '  + ' and' ' not '.join(pred_1))
 # print(g)
-
-t = ' or '.join(pred_1)
-
-result = not(print(t)) #== ((print(g) and (not Y) and (not Z))
-
-
+# t = ' or '.join(pred_1)
+# print(t)
+# result = not(t) == g
+# print(result)
 
 
+import string
+
+letters = string.ascii_uppercase
+rand_string = ' '.join(random.sample(letters, 3))
+print(rand_string)
+i=0
+rand_string[0] = random.randint(0,2)
+print(rand_string[i])
 
 
-
-
+# for i in range(lenf-1):
+#     rand_string = i
+#     print(rand_string)
+# n = random.randint(0,1)
+# print(n)
