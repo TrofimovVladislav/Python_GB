8f9c6dcf72b2330233a001353816f7640b782a01
TrofimovVladislav <arda.yug@gmail.com>
2022-10-25 21:06:07 +0300

    Edited lec 2

--- PythonLection/lection_1.py
+++ PythonLection/lection_1.py
@@ -355,14 +355,14 @@
 # print(list_numbers)
 
 
-def fib(n):
-    if n in [1, 2]:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
     
-list = []
-for e in range(1, 10):
-    list.append(fib(e))
-print(list)
+# list = []
+# for e in range(1, 10):
+#     list.append(fib(e))
+# print(list)
 
--- PythonLection/lection_2.py
+++ PythonLection/lection_2.py
@@ -1,4 +1,25 @@
-list2 = [1, 2, 3, 5, 6, 14, 38]
-f = lambda x: x**2 
-list1= [(i, f(i)) for i in list2 if i%2==0]
-print(list1)
+# path = 'C:/GB/Python_GB/PythonLection/pytext.txt'
+
+# with open(path, 'r') as file:
+#     f = file.read().split()
+
+# z = []
+# def conv(f):
+#     return [z.append(int(i)) for i in f] 
+     
+
+# res = [(i, i**2) for i in z if not i%2]
+# print(res)
+
+
+list1 = '1 2 3 5 6 14 38'.split()
+# f = lambda x: x**2 
+# list2 = [(i, f(i)) for i in list1 if i%2==0]
+# print(list2)
+
+def select(f, col):
+    return [f(x) for x in col]
+print(select(int, list1))
+
+def wert(f, col):
+    res = [lambda x: x not x%2]
